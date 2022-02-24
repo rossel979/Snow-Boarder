651452108fc06e94985e165ad9c3a8b4c3d1aef6
David Rossello <rossel979@morrisville.edu>
2022-02-24 00:03:05 -0500

    NameSpaces & SceneManagement

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class CrashDetector : MonoBehaviour
 {
@@ -8,7 +10,7 @@ public class CrashDetector : MonoBehaviour
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("Ouch, hit my head");
+            SceneManager.LoadScene(0);
         }
     }
 }
