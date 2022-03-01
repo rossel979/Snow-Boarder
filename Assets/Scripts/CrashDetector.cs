b305c277c34b3a967b2b394f2318356b6aa04ff8
David Rossello <rossel979@morrisville.edu>
2022-03-01 02:21:33 -0500

    Take a Moment to Tune

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -7,10 +7,12 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float fltloadDelay = 0.5f;
+    [SerializeField] ParticleSystem crashEffect;
      void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Ground")
         {
+            crashEffect.Play();
             Invoke("ReloadScene", fltloadDelay);
         }
     }
